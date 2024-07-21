bdc76ff1f86506263032948991af2c0ccb98e714
Тимофей <shvedov.tim30@gmail.com>
2024-07-21 19:00:19 +0300

    +

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 import telebot
 from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 from telebot.types import Message
@@ -41,14 +44,22 @@ def otvetka2(message):
     telbot.send_message(message.from_user.id, "Я спать")
     info[message.from_user.id]["legality"] = message.text
     print(info)
+    if os.path.exists(f"{message.from_user.id}.json"):
+        pass
+    else:
+
+
+        with open (f"{message.from_user.id}.json","a+",encoding="UTF-8") as f:
+                json.dump([info[message.from_user.id]],f,ensure_ascii=False)
+
+
 
 telbot.polling()
 
 """
-создать команду по которой можно будет добавить задачу
-все задачи будут храниться в словаре инфо, добавить в словарик {3483948: "пойти погулять"} - вот такой результат нужно сделать в инфо
+Узнать что такое срезы в пайтоне (старт, стоп, степ)
 
-узнать что такое JSON 
-и вспомнить, как работать с файлами в питоне (что такое with open)
+И как отобразить все буквы через одну в этой переменной:
+x = "Tima, privet. Eto test stro4ka. Попробуй с помощью срезов достать только каждый второй символ"
 
 """
