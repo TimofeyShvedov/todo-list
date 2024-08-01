e6f3d30c9fc91c727ec2f1934566114821f11482
Тимофей <shvedov.tim30@gmail.com>
2024-08-01 19:00:11 +0300

    +

--- main.py
+++ main.py
@@ -11,16 +11,24 @@ telbot = telebot.TeleBot(BOT_ID)
 info = {}
 info2 = {}
 
+
 @telbot.message_handler(commands=["watch"])
-def pokas(message:Message):
+def pokas(message: Message):
     with open(f"{message.from_user.id}.json", "r", encoding="UTF-8") as p:
         fal = json.load(p)
+        if len(fal)==0:
+            telbot.send_message(message.from_user.id, "Бро тебе надо тренироватся")
+            return False
+
 
     result = ""
     for schet, x in enumerate(fal, start=1):
         result += f"{x['task']} {x['legality']} {schet}  \n"
 
-    telbot.send_message(message.from_user.id,f"{result}")
+    telbot.send_message(message.from_user.id, f"{result}")
+    return  True
+
+
 @telbot.message_handler(commands=["start"])
 def soobsh(message: Message):
     print(message)
@@ -33,22 +41,20 @@ def soobsh(message: Message):
     telbot.send_sticker(message.from_user.id, "CAACAgIAAxkBAAEMM81mU1B_M45YswKX4Lt-5PM9uaktQAACAQADwDZPExguczCrPy1RNQQ")
 
 
-
 @telbot.message_handler(commands=["add"])
 def soobs(message: Message):
-    info[message.from_user.id] = {"task":"","legality":""}
+    info[message.from_user.id] = {"task": "", "legality": ""}
     print(info)
     telbot.send_message(message.from_user.id, "Что хотите сделать?")
     telbot.register_next_step_handler(message, otvetka)
 
 
 def otvetka(message):
-    info[message.from_user.id]["task"]=message.text
+    info[message.from_user.id]["task"] = message.text
     print(info)
 
     telbot.send_message(message.from_user.id, "Это легально?")
     telbot.register_next_step_handler(message, otvetka2)
-
 
 
 def otvetka2(message):
@@ -60,28 +66,52 @@ def otvetka2(message):
             fal = json.load(p)
             fal.append(info[message.from_user.id])
         with open(f"{message.from_user.id}.json", "w+", encoding="UTF-8") as f:
-            json.dump( fal,f, ensure_ascii=False)
+            json.dump(fal, f, ensure_ascii=False)
     else:
 
+        with open(f"{message.from_user.id}.json", "a+", encoding="UTF-8") as f:
+            json.dump([info[message.from_user.id]], f, ensure_ascii=False)
 
-        with open (f"{message.from_user.id}.json","a+",encoding="UTF-8") as f:
-                json.dump([info[message.from_user.id]],f,ensure_ascii=False)
 
 @telbot.message_handler(commands=["del"])
 def delete(message):
-    pass
+    proverka = pokas(message)
+    if proverka:
+
+        telbot.send_message(message.from_user.id, "Какое задание хотите удалить?")
+        telbot.register_next_step_handler(message, delete2)
+
+
+def delete2(message):
+    telbot.send_message(message.from_user.id, "ок")
+    if os.path.exists(f"{message.from_user.id}.json"):
+        with open(f"{message.from_user.id}.json", "r+", encoding="UTF-8") as f:
+            spisok = json.load(f)
+
+
+            try:
+                spisok.pop(int(message.text)-1)
+            except IndexError:
+                telbot.send_message(message.from_user.id, "Такого нет?")
+                return
+
+        with open(f"{message.from_user.id}.json", "w+", encoding="UTF-8") as f:
+            json.dump(spisok,f,ensure_ascii=False)
+    else:
+        with open(f"{message.from_user.id}.json", "a+", encoding="UTF-8") as f:
+            json.dump([info2[message.from_user.id]], f, ensure_ascii=False)
+            info2.pop()
 
 
 telbot.polling()
 
 """
-Внутри функции делит, нужно сделать:
-1. Задать вопрос юзеру: "какое задание хотите удалить?"
-2. Дождаться ответа от юзера (мы уже так делали 2 раза в этом проекте)
-    - Напомню: после ожидания сделать так, чтобы мы могли перейти на некст функцию (смотреть пример с переходом из функции soobs -> otvetka)
-3. В следующей функции открыть файл джсон (скопировать) на ЧТЕНИЕ  и достать из него весь список задач
-4. Заггулить: что такое функция поп в пайтоне
-5. ПОпытаться из списка удалить то, что напишет юзер.
+Узнать что такое SQLite  в пайтоне
+найти и скопировать код, который создаёт базу данных при помощи пайтона
 
-ОН пишет нам число - мы его удаляем из списка. Задание не очень лёгкое, попытаться часть сделать самому :))
+Запросы:
+- "что такое SQLite"
+- "как создать таблицу SQLite в пайтоне"
+найти видосик на ютубе с обучением "SQLite обучение на пайтоне", посмотреть хотя бы 10 мин. это оч поможет на занаятии 
+
 """
